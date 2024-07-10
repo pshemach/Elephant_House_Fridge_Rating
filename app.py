b2ce92fb6816ea904a2ca22150e514d3d1ca74f7
Pasindu <pasindusandakan@gmail.com>
2024-07-10 14:07:50 +0530

    modified app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from eleRating.models.imageRatingModel import predict_rating
+
+app = Flask(__name__)
+
+ALLOWED_FILE_EXTENTION = ['jpg','png','jpeg']
+
+def is_allowed(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_FILE_EXTENTION
+
+
+@app.route('/predict', methods=['GET','POST'])
+def image_selection():
+    if request.method == 'POST':
+        image = request.files.get('image')
+        if image and is_allowed(image.filename):
+            try:
+                prediction = predict_rating(image)
+                return jsonify({'prediction': prediction})
+            except Exception as e:
+                return jsonify({'error': str(e)})
+        return jsonify({'error': 'Invalid image file'})
+    return jsonify({'error': 'Invalid request method'})
