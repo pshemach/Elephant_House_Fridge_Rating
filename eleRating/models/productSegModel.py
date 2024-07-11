25749d3ee366d847b0ddc5fc9198aabe1ea47a0d
Pasindu <pasindusandakan@gmail.com>
2024-07-11 13:27:38 +0530

    segmentation script added

---
+++ eleRating/models/productSegModel.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+
+model_path = r'C:\Users\PC\Desktop\test\Elephant_House_Fridge_Rating\eleRating\models\my_model_2.pt'
+
+model = YOLO(model_path)
+
+def get_products(image_path):
+    results = model.predict(image_path)
+    detected_classes=[]
+    for result in results:
+        if result.box is not None:
+            for box in result.boxes:
+                class_idx = int(box.cls)
+                detected_classes.append(result.names[class_idx])
+
+    unique_classes = set(detected_classes)
+
+    return unique_classes
