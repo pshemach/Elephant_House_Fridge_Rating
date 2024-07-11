46a4dca4887ad7061b36c80a9b9a187c965da55f
Pasindu <pasindusandakan@gmail.com>
2024-07-11 14:07:30 +0530

    seg modified

--- eleRating/models/productSegModel.py
+++ eleRating/models/productSegModel.py
@@ -8,11 +8,17 @@ def get_products(image_path):
     results = model.predict(image_path)
     detected_classes=[]
     for result in results:
-        if result.box is not None:
+        if result.boxes is not None:
             for box in result.boxes:
                 class_idx = int(box.cls)
                 detected_classes.append(result.names[class_idx])
 
     unique_classes = set(detected_classes)
-
+    print(unique_classes)
     return unique_classes
+
+# image_path = r'C:\Users\PC\Desktop\test\Elephant_House_Fridge_Rating\data\rated_6.jpg'
+
+# products = get_products(image_path)
+
+# print(products)
